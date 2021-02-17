7c9a4fc620e9f50d1a2708a0343d499ec4d69396
Johnathan MEUNIER <johnathan2693@gmail.com>
2021-02-17 15:01:51 +0100

    UI React 2021  : add router

--- 3WEB/gameoflife/UI/2021/src/App.js
+++ 3WEB/gameoflife/UI/2021/src/App.js
@@ -1,16 +1,28 @@
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import { List } from "./pages/List/List";
 import { Details } from "./pages/Details/Details";
 
+import "./App.css";
+
 const App = () => {
   return (
-    <div className="App">
-      <header>
-        <h1>Personas</h1>
-      </header>
-      <List />
-      <Details />
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Personas</h1>
+        </header>
+        <Switch>
+          <Route path="/details">
+            <Details />
+          </Route>
+          <Route path="/" exact>
+            <List />
+          </Route>
+          <Route>404</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
