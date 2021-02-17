ff0d6b008dfbe5a37d668abdba4926f92f909101
Johnathan MEUNIER <johnathan2693@gmail.com>
2021-02-17 16:47:53 +0100

    UI React 2021 : details page with style

--- 3WEB/gameoflife/UI/2021/src/pages/Details/Details.js
+++ 3WEB/gameoflife/UI/2021/src/pages/Details/Details.js
@@ -1,17 +1,40 @@
 import { useParams } from "react-router-dom";
 import { people } from "../../data.json";
 
+import "./Details.css";
+
 export const Details = () => {
   const { id } = useParams();
 
-  const data = people.find(({ guid }) => guid === id);
-
-  console.log(data);
+  const { name, company, about, address, tags, friends } = people.find(({ guid }) => guid === id);
 
   return (
     <div className="details">
       <div className="header--sub">
-        <h2>Détails de la personne</h2>
+        <h2>{name}</h2>
+      </div>
+      <div className="container">
+        <div className="info">
+          <div className="details__picture-container">
+            <img className="details__picture" src="https://picsum.photos/300" alt="" />
+          </div>
+          <h3 className="details__name">{name}</h3>
+          <h4 className="details__company">{company}</h4>
+          <p className="details__description">{about}</p>
+          <ul className="details__tags">
+            {tags.map((tag) => (
+              <li>{tag}</li>
+            ))}
+          </ul>
+          <ul className="details__friends">
+            {friends.map((friend) => (
+              <li>{friend.name}</li>
+            ))}
+          </ul>
+          <a className="details__location" href={`https://www.google.fr/maps/search/${address.replaceAll(" ", "+")}`} target="_blank">
+            Voir sur Google Maps
+          </a>
+        </div>
       </div>
     </div>
   );
