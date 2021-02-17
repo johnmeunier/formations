9ee316760ddc895b90ac2786c491284103841558
Johnathan MEUNIER <johnathan2693@gmail.com>
2021-02-17 15:31:01 +0100

    UI React 2021 : retrieve correct person

--- 3WEB/gameoflife/UI/2021/src/pages/Details/Details.js
+++ 3WEB/gameoflife/UI/2021/src/pages/Details/Details.js
@@ -1,7 +1,18 @@
-export const Details = ({ person }) => (
-  <div className="details">
-    <div className="header--sub">
-      <h2>Détails de la personne</h2>
+import { useParams } from "react-router-dom";
+import { people } from "../../data.json";
+
+export const Details = () => {
+  const { id } = useParams();
+
+  const data = people.find(({ guid }) => guid === id);
+
+  console.log(data);
+
+  return (
+    <div className="details">
+      <div className="header--sub">
+        <h2>Détails de la personne</h2>
+      </div>
     </div>
-  </div>
-);
+  );
+};
